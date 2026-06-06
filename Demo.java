c79feacd8e908d81c9b4ea61344b13d550ec2e60
Nagaveni345 <naninagaveni1438@gmail.com>
2026-06-06 11:59:36 +0530

    added java programs

---
+++ Demo.java
@@ -0,0 +1,141 @@
+/*class Demo
+{
+static void message()
+{
+System.out.println("hellO");
+}
+public static void main(String[]args)
+{
+  message();
+  message();
+}*/
+/*class Demo
+{
+static void message(String name)
+{
+System.out.println("hellO"+name);
+}
+public static void main(String[]args)
+{
+  message("ravi");
+  message("sita");
+}
+}*/
+/*class Demo
+{
+static void message(int a ,int b)
+{
+System.out.println(a+b);
+}
+public static void main(String[]args)
+{
+message(5,6);
+}
+}*/
+/*class Demo
+{
+static void square(int n)
+{
+System.out.println(n*n);
+}
+public static void main(String[]args)
+{
+square(5);
+square(6);
+}
+}*/
+/*class Demo
+{
+static void check(int n)
+{
+if(n%2==0)
+System.out.println("even");
+else
+System.out.println("odd");
+}
+public static void main(String[]args)
+{
+check(10);
+check(11);
+}
+}*/
+/*class Demo
+{
+static void max(int a,int b)
+{
+if(a>b)
+System.out.println(a);
+else
+System.out.println(b);
+}
+public static void main(String[]args)
+{
+max(28,11);
+max(49,12);
+}
+}*/
+/*class Demo
+{
+static void max(int a,int b,int c)
+{
+if(a>b&&a>c)
+System.out.println(a);
+else if(b>a&&b>c)
+System.out.println(b);
+else
+System.out.println(c);
+}
+public static void main(String[]args)
+{
+max(28,11,23);
+max(49,12,44);
+}
+}*/
+
+/*class Demo
+{
+static void table(int n)
+{
+for(int i=1;i<=10;i++)
+{
+System.out.println(n*i);
+}
+}
+public static void main(String[]args)
+{
+table(10);
+}
+}*/
+
+/*class Demo
+{
+static void table(int n)
+{
+for(int i=1;i<=10;i++)
+{
+System.out.println(n+"x"+i+"="+(n*i));
+}
+}
+public static void main(String[]args)
+{
+table(10);
+}
+}*/
+class Demo
+{
+static void cube(int n)
+{
+System.out.println(n*n*n);
+}
+public static void main(String[]args)
+{
+cube(10);
+}
+}
+
+
+
+
+
+
+
